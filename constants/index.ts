8efab2377a037459aa0d6aecb3ef2729c986be58
rupin27 <rupinmehra@umass.edu>
2023-07-30 21:11:55 -0400

    Add file: Addinf Constant: For Use cases around application.

---
+++ constants/index.ts
@@ -0,0 +1,111 @@
+export const starWarsCharacters = [
+  "Luke Skywalker",
+  "Darth Vader",
+  "Princess Leia",
+  "Han Solo",
+  "Obi-Wan Kenobi",
+  "Yoda",
+  "R2-D2",
+  "C-3PO",
+  "Chewbacca",
+  "Boba Fett",
+  "Anakin Skywalker",
+  "Padmé Amidala",
+  "Lando Calrissian",
+  "Mace Windu",
+  "Rey",
+  "Kylo Ren",
+  "Finn",
+  "Poe Dameron",
+  "Jabba the Hutt",
+  "Emperor Palpatine",
+  "Darth Maul",
+  "Count Dooku",
+  "Qui-Gon Jinn",
+  "Wedge Antilles",
+  "Ahsoka Tano",
+  "Captain Rex",
+  "Darth Sidious",
+  "Jango Fett",
+  "Grand Moff Tarkin",
+  "Greedo",
+  "Stormtrooper",
+  "Bail Organa",
+  "Watto",
+  "Kit Fisto",
+  "Ki-Adi-Mundi",
+  "Asajj Ventress",
+  "Lando Calrissian",
+  "Mace Windu",
+  "Zam Wesell",
+  "Admiral Ackbar",
+  "Nien Nunb",
+];
+  
+  export const gender = [
+    { 
+      title: "Gender", 
+      value: "" 
+    },
+    { 
+      title: "Male", 
+      value: "male" 
+    },
+    { 
+      title: "Female", 
+      value: "female" 
+    },
+  ];
+  
+  export const species = [
+    {
+      title: "Species",
+      value: "",
+    },
+    {
+      title: "Human",
+      value: "Human",
+    },
+    {
+      title: "Droid",
+      value: "Droid",
+    },
+    {
+      title: "Wookiee",
+      value: "Wookiee",
+    },
+    {
+      title: "Neimoidian",
+      value: "neimoidian",
+    },
+  ];
+  
+  export const footerLinks = [
+    {
+      title: "About",
+      links: [
+        { title: "How it works", url: "/" },
+        { title: "Featured", url: "/" },
+        { title: "Partnership", url: "/" },
+        { title: "Bussiness Relation", url: "/" },
+      ],
+    },
+    {
+      title: "Company",
+      links: [
+        { title: "Events", url: "/" },
+        { title: "Blog", url: "/" },
+        { title: "Podcast", url: "/" },
+        { title: "Invite a friend", url: "/" },
+      ],
+    },
+    {
+      title: "Socials",
+      links: [
+        { title: "Linkedin", url: "https://www.linkedin.com/in/rupinmehra/" },
+        { title: "GitHub", url: "https://github.com/rupin27" },
+        { title: "Portfolio", url: "https://rupinmehra27.com" },
+        { title: "GenAIRtify", url: "https://genairtify.cloud/" },
+      ],
+    },
+  ];
